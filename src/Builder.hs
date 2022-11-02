77bff2572a0197375ced6add039238906f3324c2
mail@example.com <mail@example.com>
2022-11-02 20:10:32 +0300

    new builder

--- src/Builder.hs
+++ src/Builder.hs
@@ -9,8 +9,31 @@ import Graph ( Graph ( .. ), Tree ( .. ) )
 import Control.Monad.Trans.State ( StateT ( .. ) )
 
 ------------------------type declaration------------------
-type Building generalTp monad = StateT ( Graph generalTp () ) monad ( Graph generalTp () )
+-- TreeBuilder is supposed to be StateT where
+-- evaluation is Tree a
+-- state is Tree a -> Tree a
+type TreeBuilder gTp monad = 
+    StateT ( Tree gTp -> Tree gTp ) monad ( Tree gTp )
 
 -------------------------features--------------------------
---- - - - - - - - - - - inside Building - - - - - - - - ---
+--- - - - - - - - - - - inside TreeBuilder - - - - - - - - ---
+-- begining of TreeBuilder
+quite :: ( Eq a, Monad b ) => TreeBuilder a b
+quite = return Fault
 
+-- build TreeBuilder
+buildBy :: ( Eq a, Monad b ) => TreeBuilder a b -> ( Tree a -> Tree a ) -> b ( Tree a )
+tb `buildBy` f = fst <$> runStateT tb f
+
+-- absorbation by TreeBuilder's evaluation
+absorbes :: ( Eq a, Monad b ) => Tree a -> Graph a () -> TreeBuilder a b
+tree `absorbes` graph = return $ foldl (<>) tree $ G.dnaOfGraph graph
+{-
+quite >>= absorbes "Tree"
+
+or
+
+do
+    tree <- quite
+    tree `absorbes` "Graph"
+-}
